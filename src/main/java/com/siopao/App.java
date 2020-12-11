1fcc985df9b3765b78d0435bafa4234c48afcb7f
Alexander Paulsell <alexander.dean.paulsell@gmail.com>
2020-12-10 20:01:33 -0800

    removed references/dependancies

--- src/main/java/com/siopao/App.java
+++ src/main/java/com/siopao/App.java
@@ -2,11 +2,7 @@ package com.siopao;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
-@EnableDiscoveryClient
-@EnableEurekaClient
 @SpringBootApplication
 public class App {
     public static void main(String[] args) {
